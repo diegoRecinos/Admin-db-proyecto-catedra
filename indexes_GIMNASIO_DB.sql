2936e690f64f89bd5679d6e053716ea3e22359a9
recinos <00182122@uca.edu.sv>
2025-11-13 13:48:33 -0600

    :construction: added relevant indexes

--- indexes_GIMNASIO_DB.sql
+++ indexes_GIMNASIO_DB.sql
@@ -1,24 +1,30 @@
-/*indices para busquedas comunes (WHERE)*/
+/*indices para busquedas comunes*/
+
+/*SOCIO */ 
 /*Buscar socios activos*/
 SELECT * FROM Socio WHERE Estado = 'Activo';
-CREATE NONCLUSTERED INDEX IX_Socio_estado_activos ON Socio(Estado);
+CREATE NONCLUSTERED INDEX idx_socio_estado_activo ON Socio(Estado);
 
-
-SELECT * FROM Pago WHERE Fecha_pago BETWEEN '2025-01-01' AND '2025-12-31';
-CREATE NONCLUSTERED INDEX IX_pago_fechapago ON Pago(Fecha_Pago);
-
-
-SELECT *
-FROM Socio
+/*Buscar socio por email*/
+SELECT * FROM Socio
 WHERE Email = 'laura.torres@email.com';
+
 CREATE UNIQUE INDEX idx_socio_email ON Socio(Email);
 
+/*PAGO*/ 
+/* Buscar pago por fecha*/
+SELECT * FROM Pago WHERE Fecha_pago BETWEEN '2025-01-01' AND '2025-12-31';
+CREATE NONCLUSTERED INDEX idx_pago_fechapago ON Pago(Fecha_Pago);
 
-/*indices para las relaciones de tablas y foreign keys para mejorar joins */
-CREATE INDEX IX_Reserva_Socio ON Reserva(id_socio);
-CREATE INDEX IX_Reserva_Grupodeclase ON Reserva(id_grupo_de_clase);
-CREATE INDEX IX_Pago_Socio ON Pago(id_socio);
-CREATE INDEX IX_Pago_Entrenador ON Pago(id_entrenador);
-CREATE INDEX IX_Pago_Grupodeclase ON Pago(id_grupo_de_clase);
-CREATE INDEX IX_Clase_Entrenador ON Clase(Id_Entrenador);
-CREATE INDEX IX_Grupodeclase_Clase ON Grupo_de_Clase(id_clase);
+/*Buscar pago por socio id*/
+CREATE NONCLUSTERED INDEX idx_pago_socio ON Pago(id_socio)
+
+/*RESERVA*/
+/*Buscar reserva por fecha*/
+CREATE NONCLUSTERED INDEX idx_reserva_fecha ON Reserva(fecha_reserva);
+
+/*Buscar reserva por socio_id*/
+CREATE NONCLUSTERED INDEX idx_reserva_socio ON Reserva(id_socio);
+
+/*Buscar reserva por id_grupo clase*/
+CREATE NONCLUSTERED INDEX idx_reserva_grupo ON Reserva(id_grupo_de_clase);
